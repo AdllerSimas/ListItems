9f07168497cb8aeffcd3938e14c600e390b9ce7d
Adller Simas <adller.eel.ufsc@gmail.com>
2020-06-13 21:59:51 -0300

    update for deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import app from './app';
 import config from './config';
 
-const { PORT } = config;
+// const { PORT } = config;
+
+const port = process.env.PORT || 5000
 
 require('dotenv').config({
     path: '.env'
 })
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
